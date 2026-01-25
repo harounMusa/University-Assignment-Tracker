be46f1b1939744b2d96a3497efad5ae654c882a1
haounMusa <HarounMusa>
2026-01-25 10:27:34 -1200

    trying to fix the sesstion problem

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,30 @@ from flask_restful import Api
 from models import db
 import config
 from flask_cors import CORS
+import os
 from routes.course_route import Courses
 from routes.course_route import Course
 from routes.assignments_route import Assignments, Assignment
 from routes.auth_routes import AdminLogin, AdminCheck, AdminLogout
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://assignments-tracker.onrender.com"}}, supports_credentials=True)
-# Session Cookie Security for local development
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://assignments-tracker.onrender.com",
+            "http://127.0.0.1:5500", # VS Code Live Server
+            "http://localhost:5500"
+        ]
+    }
+}, supports_credentials=True)
+# Detect if we are running on Render
+IS_PRODUCTION = os.getenv('RENDER') is not None
+
 app.config.update(
-    SESSION_COOKIE_SAMESITE= None, # Set to 'None' if using HTTPS and different domains
+    SESSION_COOKIE_SAMESITE='None', 
     SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SECURE=True,   # Set to True only if using HTTPS
+    # Only force Secure if we are in production (HTTPS)
+    SESSION_COOKIE_SECURE=IS_PRODUCTION,
 )
 app.config.from_object(config)
 api = Api(app)
